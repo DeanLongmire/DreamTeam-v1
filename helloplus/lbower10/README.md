2bea8f1917a6a640786c4449304eb7b0a40f7448
Logan Bowers <84914281+lbower10@users.noreply.github.com>
2023-02-13 23:08:36 -0500

    Pushing HelloPlus Files.
    Created a simple user registration and login system for Hello Plus. More to add but pushing for the night.

--- helloplus/lbower10/README.md
+++ helloplus/lbower10/README.md
@@ -1,3 +1,16 @@
 # My Hello Plus
-Logan Bowers
-COSC 340
+Logan Bowers - COSC 340
+
+# Login System
+For my helloplus assignment, I followed along with a tutorial from
+
+Web Dev Simplified on youtube: [Node.js Passport Login System](https://www.youtube.com/watch?v=-RCnNyD0L-s).
+
+In this tutorial, I created a login system that allows a user to register an account, login with that account, and then log out. The account is not yet saved to an actual database, rather it is stored locally in memory just for simplicity.
+
+I gained practice with writing code to create a webpage, redirect users to different pages, and overall practice with the structure of node.js and html. On top of this, I learned how to use nodemon to host my node.js server.
+
+As the group project progresses, some experience with CSS could allow me to style my websites how I want them as well.
+
+# How to Run: 
+* In VSCode terminal, type the command "npm run devStart" to host the server. The website URL is [localhost:3000/login](http://localhost:3000/login).
--- helloplus/lbower10/helloplus.js
+++ helloplus/lbower10/helloplus.js
@@ -1,19 +1,91 @@
-myPee = document.getElementById("myPee");
-myPee.innerHTML = "Robert is the coolest"
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
-const http = require('http');
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => userList.find(userList => userList.email === email),
+  id => userList.find(user => user.id === id)
+)
 
+const userList = []
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Harry is gay');
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', confirmAuthenticated, (req, res) =>{
+  res.render('index.ejs', {name: req.user.name})
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) =>{
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) =>{
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) =>{
+  try{
+    const hashedpwd = await bcrypt.hash(req.body.password, 10)
+    userList.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedpwd
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/login');
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+function confirmAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
 
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(3000)
