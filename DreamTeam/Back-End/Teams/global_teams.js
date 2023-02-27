4ca72b222b5ec1cd9ff5be4a785ab19415de785f
Ryan-Carnes-01 <rcarnes@vols.utk.edu>
2023-02-27 15:46:45 -0500

    Added functionality to global-teams

--- DreamTeam/Back-End/Teams/global_teams.js
+++ DreamTeam/Back-End/Teams/global_teams.js
@@ -13,21 +13,67 @@ class team_dbmanager{
         });
     };
     create(){
-        this.db.run('CREATE TABLE Teams(name, ID, parent_name, parent_ID, sport)', (err)=>{
+        this.db.run('CREATE TABLE Teams(name, ID, sport, num_players)', (err)=>{
             if(err){return console.error(err.message);}
             console.log('Created team table');
         });
     };
     drop(){
-        this.db.run('DROP TABLE Team', (err)=>{
+        this.db.run('DROP TABLE Teams', (err)=>{
             if(err){return console.error(err.message);}
             console.log('Dropped team table')
         });
     };
-    insert(){};
-    display_all(){};
-    update_name(){};
-    delete(){};
+    insert(name, ID, sport, num_players){
+        this.sql = 'INSERT INTO Teams (name, ID, sport, num_players) VALUES(?, ?, ?, ?)';
+        this.db.run(this.sql, [name, ID, sport, num_players], (err)=>{
+            if(err){return console.error(err.message);}
+            console.log('New row created in Team table')
+        });
+    };
+    display_all(){
+        this.sql = 'SELECT * FROM Teams';
+        this.db.all(this.sql, [], (err,rows)=>{
+            if(err) {
+                return console.error(err.message);
+            }
+            rows.forEach((row)=>{
+                console.log(row);
+            });
+        });
+    };
+    update_name(name, new_name){
+        this.data = [new_name, name];
+        this.sql = 'UPDATE Teams SET name = ? WHERE name = ?';
+        this.db.run(this.sql, this.data, (err)=>{
+            if(err){return console.log(err.message);}
+            console.log('Row(s) name updated');
+        });
+    };
+    update_sport(name, new_sport){
+        this.data = [new_sport, name];
+        this.sql = 'UPDATE Teams SET sport = ? WHERE name = ?';
+        this.db.run(this.sql, this.data, (err)=>{
+            if(err){return console.log(err.message);}
+            console.log('Row(s) sport updated');
+        });
+    };
+    update_num_players(name, new_num_players){
+        this.data = [new_num_players, name];
+        this.sql = 'UPDATE Teams SET num_players = ? WHERE name = ?';
+        this.db.run(this.sql, this.data, (err)=>{
+            if(err){return console.log(err.message);}
+            console.log('Row(s) sport updated');
+        });
+    };
+    delete(name){
+        this.data = [name];
+        this.sql = 'DELETE FROM Teams WHERE name = ?';
+        this.db.run(this.sql, this.data, (err)=>{
+            if(err){return console.log(err.message);}
+            console.log('Row(s) deleted')
+        });
+    };
     close(){
         this.db.close((err) => {
             if(err){
--- DreamTeam/Back-End/main.js
+++ DreamTeam/Back-End/main.js
@@ -1,13 +1,12 @@
-//Requires
 const sqlite3 = require('sqlite3').verbose();
 const global_leagues = require('./Leagues/global_leagues'); //Custom import
-const global_teams = require('./Teams/global_teams');
+const global_teams = require('./Teams/global_teams'); //Custom import
 
 let league_db = new global_leagues.league_dbmanager;
 let team_db = new global_teams.team_dbmanager;
 
-league_db.open();
-console.log("League Opened");
+//TESTING
+//league_db.open();
 //league_db.create();
 //league_db.insert("UTKFF", 1, "Flag Football");
 //league_db.insert("UTKS", 2, "Soccer");
@@ -19,21 +18,15 @@ console.log("League Opened");
 //league_db.delete("UTKFF");
 //league_db.display_all();
 //league_db.drop();
-league_db.close();
-console.log("League Closed");
+//league_db.close();
 
-team_db.open();
-console.log("Team Opened");
+
+//team_db.open();
 //team_db.create();
-//team_db.insert("UTKFF", 1, "Flag Football");
-//team_db.insert("UTKS", 2, "Soccer");
-//team_db.insert("UTKBB", 3, "Basketball");
-//team_db.insert("UTKT", 4, "Tennis");
-//team_db.insert("UTKBB2", 5, "Basketball");
-//team_db.update_sport("UTKBB", "Archery");
-//team_db.update_name("UTKBB2", "NBA-Official");
-//team_db.delete("UTKFF");
+//team_db.insert("The Eagles of Gondor", 1, "Football", 15);
+//team_db.update_sport("The Eagles of Gondor", "Basketball");
+//team_db.update_name("The Eagles of Gondor", "The Uruks of Mordor");
+//team_db.delete("The Uruks of Mordor");
 //team_db.display_all();
 //team_db.drop();
-team_db.close();
-console.log("Team Closed");
+//team_db.close();
