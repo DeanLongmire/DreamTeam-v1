9cebbcbdbc37bb35f754cfd24abab9e1363883ed
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-03-18 21:16:42 -0400

    Login Fields 8
    Button initially disabled, if type username then type password button becomes enabled - working on small bug

--- DreamTeam/Front-End/login.js
+++ DreamTeam/Front-End/login.js
@@ -1,6 +1,6 @@
 /*Login JS to make users complete all fields have to fix functionality
 still moves on even if fields aren't actually filled*/
-document.getElementById("Login").onclick = function() {
+/*document.getElementById("Login").onclick = function() {
     let allAreFilled = true;
     document.getElementById("sign-in").querySelectorAll("[required]").forEach(function(i) {
     if (!allAreFilled) return;
@@ -9,6 +9,7 @@ document.getElementById("Login").onclick = function() {
     document.getElementById("sign-in").querySelectorAll(`[name=${i.name}]`).forEach(function(r) {
     if (r.checked) radioValueCheck = true;
     })
+      $('#Login').prop('disabled', false);
       allAreFilled = radioValueCheck;
       return;
     }
@@ -16,5 +17,21 @@ document.getElementById("Login").onclick = function() {
   })
   if (!allAreFilled) {
     alert('Fill all the fields');
+    $('#Login').prop('disabled', true);
   }
-};
+};*/
+
+
+let input = document.querySelector(".dynprog-input");
+let button = document.querySelector(".Login");
+Login.disabled = true;
+input.addEventListener("change", stateHandle);
+
+function stateHandle(){
+  if(document.querySelector(".dynprog-input").value === ""){
+    Login.disabled = true;
+  }
+  else{
+    Login.disabled = false;
+  }
+}
