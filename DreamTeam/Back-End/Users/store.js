19f4ee52f0c0863bf10eff4c03209ef245092e55
jlongmi9 <jlongmi9@vols.utk.edu>
2023-03-21 09:20:55 -0400

    Working on HTTP request

---
+++ DreamTeam/Back-End/Users/store.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/store-data', function(req, res) {
+  const name = req.body.name;
+
+  // Store the name in your database here
+
+  res.status(200).send('Data stored successfully');
+});
+
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
