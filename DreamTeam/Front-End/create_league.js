159db75f36e7294b8186cc249f1d3bcdaa81c378
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-03-25 21:06:38 -0400

    Work on JS for create league to send data back to node js
    Added JS code to create league page as well as fix formatting

--- DreamTeam/Front-End/create_league.js
+++ DreamTeam/Front-End/create_league.js
@@ -13,3 +13,33 @@
      create_league.disabled = false;
    }
  }
+
+ /*Take the sport that is selected & the league name and 
+ put them in a json object*/
+
+ const url = 'http://localhost:5000/leagues' ;
+
+ let saveLeague = () => {
+    //Get data from each element
+    const sport = document.getElementById("sport");
+    const leagueName = document.getElementById("league_name");
+
+    //This variable stores all the data
+    let data = "\r Sport: " + sport.value + " \r \n" 
+        + "League Name: " + leagueName.value;
+
+ }
+
+
+/*This is starting to set up how to send the data back to 
+the nodejs server using fetch and catch in order to do this*/
+ fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+ })
+ .then(response => response.json())
+ .then(data => console.log(data))
+ .catch(error => console.error(error));
