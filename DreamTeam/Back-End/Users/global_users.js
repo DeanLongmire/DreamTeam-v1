f9b41c976f3e9ced3c63f6438686d84cc0702056
jlongmi9 <107720531+DeanLongmire@users.noreply.github.com>
2023-03-26 23:37:50 -0400

    Changed Controllers/global_users.js
    Added some update functions and changed some route names

--- DreamTeam/Back-End/Users/global_users.js
+++ DreamTeam/Back-End/Users/global_users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getUser, createUser} = require('../controllers/global_users.js');
+const { get_user, create_user, update_username, update_email } = require('../controllers/global_users.js');
 
 const router = express.Router();
 
@@ -10,7 +10,9 @@ router.get('/', (req, res) => {
   res.send("Test");
 });
 
-router.get('/:id', getUser);
-router.post('/', createUser)
+router.get('/:id', get_user);
+router.post('/', create_user)
+router.patch('/update_username/:id', update_username)
+router.patch('/update_email/:id', update_email)
 
 module.exports = router;
