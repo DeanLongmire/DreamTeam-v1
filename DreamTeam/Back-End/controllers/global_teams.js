258e0820acceb2e75a28786a10f1eab83fad35bf
Logan Bowers <84914281+lbower10@users.noreply.github.com>
2023-03-27 23:21:10 -0400

    Update global_teams.js
    Put get_path() function beginning in the correct spot

--- DreamTeam/Back-End/controllers/global_teams.js
+++ DreamTeam/Back-End/controllers/global_teams.js
@@ -45,15 +45,15 @@ const show_all = (req, res) => {
 
 // create a team
 const create_team = (req, res) => {
-    const team = req.body;
-    const teamId = uuidv4();
-  
-    //adds unique ID to the team (team W/ ID)
-    const teamWid = { ... team, id: teamId}
-
-    console.log(teamWid);
-
     get_path( (path) => {
+        const team = req.body;
+        const teamId = uuidv4();
+  
+        //adds unique ID to the team (team W/ ID)
+        const teamWid = { ... team, id: teamId}
+
+        console.log(teamWid);
+    
         db.open(path);
         db.insert(teamWid.name, teamWid.ID, teamWid.P_ID, teamWid.sport, teamWid.num_players,() =>{
             db.close();
