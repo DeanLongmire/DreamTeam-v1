778ac6c3efc1f907739ae1429d0cf8af738a9b6c
Finli-Hill <122070082+Finli-Hill@users.noreply.github.com>
2023-04-06 20:49:33 -0500

    Requirements for Passwords
    Did a few things here:
    1. Added JS that checks for a password
        that contains at least 8 characters
        and has at least one number 0-9.
    2. Added two error messages to tell
        the user why the passwords aren't
        valid (changes to css and html)
    3. Made some JS that masks the users
        password input with '*' while they
        are typing.
    
    Final note for Back-End,:
       I'm leaving in my old JS so that
       if anything I changed screwed with
       the back-end we can revert to fix it.
       It shouldn't, but who knows.

--- DreamTeam/Front-End/users/Registry.js
+++ DreamTeam/Front-End/users/Registry.js
@@ -1,20 +1,94 @@
-//First section is just for form checking
-var form = document.getElementById("regform");
-var registerButton = document.getElementById("create_account");
-var passwordInput = document.getElementById("psw");
-var confirmPasswordInput = document.getElementById("psw-repeat");
+/*//----------------------------------------------------First section is just for form checking
+// var form = document.getElementById("regform");
+// var registerButton = document.getElementById("create_account");
+// var passwordInput = document.getElementById("psw");
+// var confirmPasswordInput = document.getElementById("psw-repeat");
+// var errorElement = document.getElementById("password-error-message");
+
+// registerButton.setAttribute("disabled", "disabled");
+
+// function checkFormInputs() {
+//   var inputs = form.querySelectorAll("input");
+//   for (var i = 0; i < inputs.length; i++) {
+//     if (inputs[i].value === "") {
+//       return false; // Input is empty, return false
+//     }
+//   }
+//   if( passwordInput.value !== confirmPasswordInput.value) {
+//     var errorMessage = "Passwords Do Not Match"; //error message for invalid password
+//     errorElement.innerText = errorMessage;
+//     errorElement.style.display = "block"
+//     return false; // Passwords do not match, return false
+//   }
+//   var passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z\d]{8,}$/;// Regex to match passwords containing at least one digit and being at least 8 characters long
+//   if (!passwordRegex.test(passwordInput.value)) {
+//     var errorMessage = "Password must be at least 8 characters long and contain at least one number."; //error message for invalid password
+//     errorElement.innerText = errorMessage;
+//     errorElement.style.display = "block"
+//     return false; //Password does not meet the requirements, return false
+//   }
+//   return true; // All inputs are filled, return true
+// }
+
+// function handleButtonState() {
+//   if (checkFormInputs()) {
+//     registerButton.removeAttribute("disabled");
+//     var errorElement = document.getElementById("password-error-message");
+//     errorElement.style.display = "none"; // Hide the error message
+//   } else {
+//     registerButton.setAttribute("disabled", "disabled");
+//   }
+// }
+
+// // Add event listeners to form inputs
+// var inputs = form.querySelectorAll("input");
+// for (var i = 0; i < inputs.length; i++) {
+//   inputs[i].addEventListener("input", handleButtonState);
+// }
+
+// // Add event listener to password input field, this masks the inputs with '*'
+// passwordInput.addEventListener("input", function() {
+//   const password = passwordInput.value;
+//   const maskedPassword = '*'.repeat(password.length - 1) + password.charAt(password.length - 1);
+//   passwordInput.value = maskedPassword;
+
+//   var passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z\d]{8,}$/;
+//   if (passwordRegex.test(password)) {
+//     errorElement.style.display = "none"; // Hide the error message
+//   }
+// });
+// //same for password repeat
+// confirmPasswordInput.addEventListener("input", function() {
+//   const confirmPassword = confirmPasswordInput.value;
+//   const confirmMaskedPassword = '*'.repeat(confirmPassword.length - 1) + confirmPassword.charAt(confirmPassword.length - 1);
+//   confirmPasswordInput.value = confirmMaskedPassword;
+// });
+*/
+const form = document.getElementById("regform");
+const registerButton = document.getElementById("create_account");
+const passwordInput = document.getElementById("psw");
+const confirmPasswordInput = document.getElementById("psw-repeat");
+const errorElement = document.getElementById("password-error-message");
 
 registerButton.setAttribute("disabled", "disabled");
 
 function checkFormInputs() {
-  var inputs = form.querySelectorAll("input");
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].value === "") {
+  const inputs = form.querySelectorAll("input");
+  for (const input of inputs) {
+    if (input.value === "") {
       return false; // Input is empty, return false
     }
   }
   if (passwordInput.value !== confirmPasswordInput.value) {
+    errorElement.innerText = "*Passwords do not match*";
+    errorElement.style.display = "block";
     return false; // Passwords do not match, return false
+  }
+  const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z\d]{8,}$/; // Regex to match passwords containing at least one digit and being at least 8 characters long
+  if (!passwordRegex.test(passwordInput.value)) {
+    errorElement.innerText = "*Password must be at least 8 characters long and contain at least one number.*";
+    errorElement.style.display = "block";
+    return false; //Password does not meet the requirements, return false
   }
   return true; // All inputs are filled, return true
 }
@@ -22,19 +96,34 @@ function checkFormInputs() {
 function handleButtonState() {
   if (checkFormInputs()) {
     registerButton.removeAttribute("disabled");
+    errorElement.style.display = "none"; //Hide the error message
   } else {
     registerButton.setAttribute("disabled", "disabled");
   }
 }
 
 // Add event listeners to form inputs
-var inputs = form.querySelectorAll("input");
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("input", handleButtonState);
+const inputs = form.querySelectorAll("input");
+for (const input of inputs) {
+  input.addEventListener("input", handleButtonState);
 }
 
+// Add event listener to password input field, this masks the inputs with '*'
+passwordInput.addEventListener("input", function() {
+  const password = passwordInput.value;
+  const maskedPassword = '*'.repeat(password.length - 1) + password.charAt(password.length - 1);
+  passwordInput.value = maskedPassword;
+});
 
-//this section is for passing information to the back-end
+// Same for password repeat
+confirmPasswordInput.addEventListener("input", function() {
+  const confirmPassword = confirmPasswordInput.value;
+  const confirmMaskedPassword = '*'.repeat(confirmPassword.length - 1) + confirmPassword.charAt(confirmPassword.length - 1);
+  confirmPasswordInput.value = confirmMaskedPassword;
+});
+
+
+//---------------------------this section is for passing information to the back-end
 
 //const url = 'https://localhost:5000/users';
 const url = 'http://localhost:5000/users';
