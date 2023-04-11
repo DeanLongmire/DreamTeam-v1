96422791bf6db0aadbf0ca47d6dfb75e53444b10
jlongmi9 <107720531+DeanLongmire@users.noreply.github.com>
2023-04-11 00:06:48 -0400

    Added getting cookie info to Profile Page
    Definitely a next sprint issue but now we have a way of keeping track of the user logged in

--- DreamTeam/Front-End/users/profile.js
+++ DreamTeam/Front-End/users/profile.js
@@ -1,6 +1,15 @@
 /*If user selects log out, a message says they have been logged out
 then they return back to the home page*/
 
+//COOKIE INFO
+const cookies = document.cookie.split(';');
+const cookie = cookies.find(c => c.trim().startsWith('myCookie='));
+const userCookieId = cookie ? cookie.split('=')[1] : null;
+console.log(userCookieId);
+
+//need to send this userCookieId to the client side, get the user's data, send it back to this page, and then display it appropriatly
+//Also need to probably halt the page from loading all its elements while this happens
+
 document.querySelector("#Log-Out").onclick = function(){
     alert("You have been logged out");
 }
