ef0215530f9c16dc90d550532de1272facac22af
Finli-Hill <122070082+Finli-Hill@users.noreply.github.com>
2023-04-12 20:09:57 -0400

    Changes to home page,
    I will make a dyno move across the screen followed by his friends but for now its two shapes moving back and forth :ambulance:

--- DreamTeam/Front-End/home/index.js
+++ DreamTeam/Front-End/home/index.js
@@ -1,34 +1,37 @@
 //script code for the home page
+// Define variables for the dinosaur element and the buttons
+var dino = document.getElementById("dinosaur");
+var buttons = document.querySelectorAll(".dynprog-homebutton");
 
+// Define initial position of dinosaur and distance to travel
+var currentPosition = 0;
+var distanceToTravel = window.innerWidth - 100;
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("dynprog-fade");
-  let dots = document.getElementsByClassName("dynprog-dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}   /*sets it back to one*/
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+// Move dinosaur across the screen and make it hop over each button
+function moveDinosaur() {
+  if (currentPosition < distanceToTravel) {
+    currentPosition += 10;
+    dino.style.left = currentPosition + "px";
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      var buttonPosition = button.getBoundingClientRect();
+      var dinoPosition = dino.getBoundingClientRect();
+      if (dinoPosition.left >= buttonPosition.left && dinoPosition.left <= buttonPosition.right && dinoPosition.bottom >= buttonPosition.top) {
+        dino.classList.add("jump");
+      } else {
+        dino.classList.remove("jump");
+      }
+    }
+  } else {
+    currentPosition = 0;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  /*makes it so the slides loop*/
-  dots[slideIndex-1].className += " active";
+  window.requestAnimationFrame(moveDinosaur);
 }
 
+// Call moveDinosaur function to start animation
+moveDinosaur();
 
+// navbar stuff
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -36,3 +39,14 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "0";
   }
 }
+window.addEventListener('scroll', () => {
+      const footer = document.querySelector('.dynprog');
+      const containerHeight = document.querySelector('.container').offsetHeight;
+      const currentScroll = window.pageYOffset;
+      const windowHeight = window.innerHeight;
+      if (currentScroll + windowHeight >= containerHeight) {
+        footer.classList.add('show');
+      } else {
+        footer.classList.remove('show');
+      }
+    });
