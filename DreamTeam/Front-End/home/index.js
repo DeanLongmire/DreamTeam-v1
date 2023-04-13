f6bc54454f2c65c2d0eecf74eee953fa3fadc937
Finli-Hill <122070082+Finli-Hill@users.noreply.github.com>
2023-04-12 21:27:35 -0400

    HAHAHAH
    HAHAHAH

--- DreamTeam/Front-End/home/index.js
+++ DreamTeam/Front-End/home/index.js
@@ -1,35 +1,4 @@
-//script code for the home page
-// Define variables for the dinosaur element and the buttons
-var dino = document.getElementById("dinosaur");
-var buttons = document.querySelectorAll(".dynprog-homebutton");
 
-// Define initial position of dinosaur and distance to travel
-var currentPosition = 0;
-var distanceToTravel = window.innerWidth - 100;
-
-// Move dinosaur across the screen and make it hop over each button
-function moveDinosaur() {
-  if (currentPosition < distanceToTravel) {
-    currentPosition += 10;
-    dino.style.left = currentPosition + "px";
-    for (var i = 0; i < buttons.length; i++) {
-      var button = buttons[i];
-      var buttonPosition = button.getBoundingClientRect();
-      var dinoPosition = dino.getBoundingClientRect();
-      if (dinoPosition.left >= buttonPosition.left && dinoPosition.left <= buttonPosition.right && dinoPosition.bottom >= buttonPosition.top) {
-        dino.classList.add("jump");
-      } else {
-        dino.classList.remove("jump");
-      }
-    }
-  } else {
-    currentPosition = 0;
-  }
-  window.requestAnimationFrame(moveDinosaur);
-}
-
-// Call moveDinosaur function to start animation
-moveDinosaur();
 
 // navbar stuff
 window.onscroll = function() {scrollFunction()};
