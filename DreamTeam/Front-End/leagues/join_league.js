2052caf2b7b4e401ccc2c7ed81fe4746a8b59812
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-04-18 14:59:41 -0400

    Work on View Leagues

--- DreamTeam/Front-End/leagues/join_league.js
+++ DreamTeam/Front-End/leagues/join_league.js
@@ -1,8 +1,15 @@
 // Array of league names
 const leagues = ["League A", "League B", "League C"];
 
+const ls =[
+    {name: 'League 1', team: 1.99},
+    {name: 'League 2', team: 2.99},
+    {name: 'League 3', team: 3.99}
+];
+
 // Get the container element where the league names will be displayed
 const leagueContainer = document.getElementById("league_container");
+const lContainer = document.getElementById("l_container");
 
 // Loop through the array of league names and create a new h4 element for each one
 for (let i = 0; i < leagues.length; i++) {
@@ -11,3 +18,10 @@ for (let i = 0; i < leagues.length; i++) {
     h4.textContent = leagueName;
     leagueContainer.appendChild(h4);
 }
+
+for (const l of ls) {
+    const div = document.createElement('div');
+    div.classList.add('.dynprog-better-box');
+    div.innerHTML = `<h3>${l.name}</h3><p>$${l.team.toFixed(2)}</p>`;
+    container.appendChild(div);
+  }
