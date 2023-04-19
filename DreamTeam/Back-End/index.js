e489028031aab9d94ca7f749c959ae8365920481
jlongmi9 <107720531+DeanLongmire@users.noreply.github.com>
2023-04-19 18:55:49 -0400

    Removed Unused Stuff

--- DreamTeam/Back-End/index.js
+++ DreamTeam/Back-End/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-//const sqlite = require("better-sqlite3");
-//const SqliteStore = require("better-sqlite3-session-store")(session)
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const { v4: uuidv4 } = require('uuid');
 
 //Routes
 const usersRoutes = require('./Users/global_users.js');
@@ -15,23 +12,10 @@ const players = require('./Players/global_players.js');
 const app = express();
 const PORT = 5000;
 
-//set up session store
-/*const db = new sqlite("DreamTeam/Back-End/sessions.db", { verbose: console.log });
-let sessionStore = new SqliteStore({
-    client: db, 
-    expired: {
-      clear: true,
-      intervalMs: 900000 //ms = 15min
-    }
-});*/
-
-//sets up user sessions
-//sessID = uuidv4();
 app.use(session({
     secret: 'my secret key',
     resave: false,
     saveUninitialized: true,
-    //store: sessionStore,
     cookie: {
         httpOnly: false,
         secure: false,
