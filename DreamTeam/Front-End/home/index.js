e4ede7bd874acc778a4b5da15400b77df6120105
jlongmi9 <107720531+DeanLongmire@users.noreply.github.com>
2023-04-24 15:52:58 -0400

    Added stuff to check if user is logged in on home page

--- DreamTeam/Front-End/home/index.js
+++ DreamTeam/Front-End/home/index.js
@@ -1,4 +1,36 @@
+let loggedIn;
 
+let getSessionId = function (callback) {
+  const cookies = document.cookie.split(';');
+  const cookie = cookies.find(c => c.trim().startsWith('UserCookie'));
+  const userCookieId = cookie ? cookie.split('=')[1] : null;
+  console.log(userCookieId);
+
+  const sessionId = {
+    id: userCookieId
+  }
+
+  if(userCookieId === null)
+  {
+    loggedIn = 0;
+  }
+  else
+  {
+    loggedIn = 1;
+  }
+
+  const userURL = 'http://127.0.0.1:5000/users/' + sessionId.id;
+
+  callback(userURL);
+}
+
+getSessionId((userURL) => {
+  if(loggedIn === 1)
+  {
+    //CODE OR FUNCTION HERE TO HIDE LOGIN/REGISTER 
+    //ALSO ADD STUFF TO HOME BAR FOR GOING TO LEAGUE AND TEAM AND PROFILE
+  }
+});
 
 // navbar stuff
 window.onscroll = function() {scrollFunction()};
