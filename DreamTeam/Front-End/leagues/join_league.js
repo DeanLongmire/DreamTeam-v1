8a3d4a3c25a815b8f7a97eb49d7322b3a83612e6
Ryan-Carnes-01 <rcarnes@vols.utk.edu>
2023-05-04 18:24:06 -0400

    Update join_league.js

--- DreamTeam/Front-End/leagues/join_league.js
+++ DreamTeam/Front-End/leagues/join_league.js
@@ -1,7 +1,7 @@
 // Array of league names
-const leagues = ["League A", "League B", "League C"];
+//var leagues = ["League A", "League B", "League C"];
 let userCookieId;
-
+/*
 const ls =[
     {name: 'League 1', team: 1.99},
     {name: 'League 2', team: 2.99},
@@ -27,101 +27,75 @@ for (const l of ls) {
     //container.appendChild(div);
 }
 
-
+*/
 ///COOKIE INFO
 let getSessionId = function (callback) {
-  const cookies = document.cookie.split(';');
-  const cookie = cookies.find(c => c.trim().startsWith('UserCookie'));
-  const userCookieId = cookie ? cookie.split('=')[1] : null;
-  console.log(userCookieId);
-
-  const sessionId = {
-    id: userCookieId
-  }
-
-  const userURL = 'http://127.0.0.1:5000/users/' + sessionId.id;
-
-  callback(userURL);
-}
-
-let getUserData = function (url,callback) {
-  console.log(url);
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(response => {
-    if (response.ok) {
-      response.json().then(data => {
-        setUserData(data, () => {
-          console.log("User Data Set");
-          const teamURL = 'http://127.0.0.1:5000/teams/' + data.teamID;
-            if(data.teamID !== null) 
-            {
-              getTeamData(teamURL, () => {
-                callback();
-              });
-            }
-            else
-            {
-              callback();
-            }
-        });
-      });
-    } 
-    else {
-      console.error('Error: ' + response.statusText);
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
-}
-
-let getTeamData = function (teamURL, callback) {
-  fetch(teamURL, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-  }
-  })
-  .then(response => {
-    if (response.ok) {
-      response.json().then(data => {
-        setTeamData(data, () => {
-          console.log("Team Data Set");
-          const leagueURL = 'http://127.0.0.1:5000/leagues/' + data.p_id;
-          getLeagueData(leagueURL, () => {
-            callback();
-          });
-        });
-      });
-    } 
-    else {
-      console.error('Error: ' + response.statusText);
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
-}
+    const cookies = document.cookie.split(';');
+    const cookie = cookies.find(c => c.trim().startsWith('UserCookie'));
+    userCookieId = cookie ? cookie.split('=')[1] : null;
+    console.log(userCookieId);
   
-  let getLeagueData = function (leagueURL, callback) {
-    console.log("in get leage data");
+    const sessionId = {
+      id: userCookieId
+    }
+  
+    const userURL = 'http://127.0.0.1:5000/users/' + sessionId.id;
+  
+    callback(userURL);
+  }
+  let setLeagueData = function(LeagueDataJSON, callback){
+    const leagueContainer = document.getElementById("league_container");
+    const lContainer = document.getElementById("l_container");
+    names = LeagueDataJSON.names;
+    sports = LeagueDataJSON.sports;
+    console.log(names);
+    console.log(sports);
+    for (let i = 0; i < names.length; i++) {
+      const leagueName = names[i];
+      const leagueSport = sports[i];
+      const h4 = document.createElement("h4");
+      h4.textContent = leagueName + ": " + leagueSport;
+      leagueContainer.appendChild(h4);
+    }
+    callback();
+  } 
+  let getLeagueData = function(leagueURL, callback){
+    console.log(leagueURL);
     fetch(leagueURL, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
-    }
+      }
+    })
+    .then(response => {
+      if(response.ok){
+        response.json().then(data =>{
+          setLeagueData(data, ()=>{
+            console.log("set league data")
+          })
+        })
+      }
+    })
+    callback();
+  }
+  let getUserData = function (url,callback) {
+    console.log(url);
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
     .then(response => {
       if (response.ok) {
         response.json().then(data => {
-          setLeagueData(data, () => {
-            console.log("League Data Set");
-            callback();
+          setUserData(data, () => {
+            console.log("User Data Set");
+            //CONTINUE FROM HERE
+            leagueURL = "http://127.0.0.1:5000/leagues/"
+            getLeagueData(leagueURL,()=>{
+              console.log("League Data Set");
+            })
           });
         });
       } 
@@ -132,6 +106,7 @@ let getTeamData = function (teamURL, callback) {
     .catch(error => {
       console.error(error);
     });
+    callback();
   }
 
   let loadData = function () {
@@ -139,21 +114,10 @@ let getTeamData = function (teamURL, callback) {
       console.log("URL: " + userURL);
       getUserData(userURL,() => {
         console.log("All Data Set");
+
       });
     });
   };
-
-  let setLeagueData = function (leagueDataJSON){
-    console.log("In the setleague function");
-    for(let i=0; i < leagueDataJSON.length; i++){
-       const leagueDiv = document.createElement("div");
-       leagueContainer.textContent = leagueDataJSON[i].name;
-       leagueContainer.appendChild(leagueDiv);
-
-    }
-    console.log(leagueDataJSON.name);
-    console.log(leagueDataJSON.sport);
-  }
 
 const welcomeButton = document.querySelector("#welcome-button");
   //Reference for username
