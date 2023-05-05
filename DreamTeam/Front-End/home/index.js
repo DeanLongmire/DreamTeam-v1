605852117191bb7c1c4e1b57cf7adef28c9039f9
Finli-Hill <122070082+Finli-Hill@users.noreply.github.com>
2023-05-04 20:50:44 -0400

    about us
    Finally have the about us made

--- DreamTeam/Front-End/home/index.js
+++ DreamTeam/Front-End/home/index.js
@@ -157,17 +157,54 @@ function setSlidePosition() {
 
 setSlidePosition();
 
-/*about us*/
-function toggleCollapse() {
-  const aboutUsSection = document.getElementById("aboutUsSection");
-  const collapseButton = document.querySelector(".collapse-button");
+// /*about us*/
+// function toggleCollapse() {
+//   const aboutUsSection = document.getElementById("aboutUsSection");
+//   const collapseButton = document.querySelector(".collapse-button");
 
-  if (aboutUsSection.style.maxHeight) {
-    aboutUsSection.style.maxHeight = null;
-    collapseButton.classList.remove("expanded");
+//   if (aboutUsSection.style.maxHeight) {
+//     aboutUsSection.style.maxHeight = null;
+//     collapseButton.classList.remove("expanded");
+//   } else {
+//     aboutUsSection.style.maxHeight = aboutUsSection.scrollHeight + "px";
+//     collapseButton.classList.add("expanded");
+//   }
+// }
+
+function toggleCollapse(elementId, button) {
+  const section = document.getElementById(elementId);
+  const aboutUsSection = document.getElementById("aboutUsSection");
+
+  if (section.style.maxHeight) {
+    section.style.maxHeight = null;
+    button.classList.remove("expanded");
   } else {
-    aboutUsSection.style.maxHeight = aboutUsSection.scrollHeight + "px";
-    collapseButton.classList.add("expanded");
+    section.style.maxHeight = section.scrollHeight + "px";
+    button.classList.add("expanded");
+  }
+
+  // Update the height of the aboutUsSection
+  updateAboutUsSectionHeight(button);
+}
+
+function updateAboutUsSectionHeight(aboutUsButton) {
+  const aboutUsSection = document.getElementById("aboutUsSection");
+  const subDropdowns = document.querySelectorAll(".sub-dropdown-content");
+
+  let totalHeight = 0;
+
+  subDropdowns.forEach((dropdown) => {
+    if (dropdown.style.maxHeight) {
+      totalHeight += parseInt(dropdown.style.maxHeight);
+    }
+  });
+
+  // Check if the aboutUsButton is expanded
+  if (aboutUsButton && aboutUsButton.classList.contains("expanded")) {
+    // Add initial height value to accommodate the sub-dropdown buttons
+    aboutUsSection.style.maxHeight = (totalHeight + subDropdowns.length * 70) + "px";
+  } else {
+    aboutUsSection.style.maxHeight = null;
   }
 }
 
--- DreamTeam/Front-End/home/test/test.js
+++ DreamTeam/Front-End/home/test/test.js
@@ -1,57 +1,36 @@
-const teamList = document.getElementById('team-container');
-// const sqlite3 = require('sqlite3').verbose();
-// const path = require('path');
-// const tdb = new sqlite3.Database(path.join(__dirname, '../Teams/global_teams_db.db'));
-const teams = [
-  { name: 'Team A', sport: 'Flag Football', num_players: 10, W: 5, L: 2 },
-  { name: 'Team B', sport: 'Men\'s Soccer', num_players: 11, W: 7, L: 3 },
-  { name: 'Team C', sport: 'Softball', num_players: 9, W: 3, L: 4 },
-  { name: 'Team D', sport: 'Flag Football', num_players: 10, W: 5, L: 2 },
-  { name: 'Team E', sport: 'Men\'s Soccer', num_players: 11, W: 7, L: 3 },
-  { name: 'Team F', sport: 'Softball', num_players: 9, W: 3, L: 4 },
-  { name: 'Team G', sport: 'Flag Football', num_players: 10, W: 5, L: 2 },
-  { name: 'Team H', sport: 'Men\'s Soccer', num_players: 11, W: 7, L: 3 },
-  { name: 'Team I', sport: 'Softball', num_players: 9, W: 3, L: 4 },
-  { name: 'Team J', sport: 'Flag Football', num_players: 10, W: 5, L: 2 },
-  { name: 'Team K', sport: 'Men\'s Soccer', num_players: 11, W: 7, L: 3 },
-  { name: 'Team L', sport: 'Softball', num_players: 9, W: 3, L: 4 },
-  // add more teams here
-];
-//This should make it so the boxes don't jumble and get moved around when hoverd over
-function expandBox(teamElement) {
-  const card = teamElement.querySelector('.dynprog-crd');
-  card.style.height = "200px";
+function toggleCollapse(elementId, button) {
+  const section = document.getElementById(elementId);
+  const aboutUsSection = document.getElementById("aboutUsSection");
+
+  if (section.style.maxHeight) {
+    section.style.maxHeight = null;
+    button.classList.remove("expanded");
+  } else {
+    section.style.maxHeight = section.scrollHeight + "px";
+    button.classList.add("expanded");
+  }
+
+  // Update the height of the aboutUsSection
+  updateAboutUsSectionHeight(button);
 }
 
-function collapseBox(teamElement) {
-  const card = teamElement.querySelector('.dynprog-crd');
-  card.style.height = "100px";
+function updateAboutUsSectionHeight(aboutUsButton) {
+  const aboutUsSection = document.getElementById("aboutUsSection");
+  const subDropdowns = document.querySelectorAll(".sub-dropdown-content");
+
+  let totalHeight = 0;
+
+  subDropdowns.forEach((dropdown) => {
+    if (dropdown.style.maxHeight) {
+      totalHeight += parseInt(dropdown.style.maxHeight);
+    }
+  });
+
+  // Check if the aboutUsButton is expanded
+  if (aboutUsButton && aboutUsButton.classList.contains("expanded")) {
+    // Add initial height value to accommodate the sub-dropdown buttons
+    aboutUsSection.style.maxHeight = (totalHeight + subDropdowns.length * 70) + "px";
+  } else {
+    aboutUsSection.style.maxHeight = null;
+  }
 }
-
-//Loop through teams and create elements for each one
-for (let i = 0; i < teams.length; i++) {
-const team = teams[i];
-const teamElement = document.createElement('div');
-teamElement.className = 'dynprog-col dynprog-third dynprog-margin-bottom';
-teamElement.innerHTML = `
-  <div class="dynprog-crd dynprog-hover-shadow">
-    <div class="dynprog-container dynprog-center">
-      <h4><b>${team.name}</b></h4> 
-      <p>${team.sport}</p> 
-      <p class="team-info" style="display: none;">Players: ${team.num_players}, Wins: ${team.W}, Losses: ${team.L}</p>
-    </div>
-  </div>
-`;
-teamList.appendChild(teamElement);
-
-// Add event listener to show/hide team info on hover
-teamElement.addEventListener('mouseenter', () => {
-  const teamInfo = teamElement.querySelector('.team-info');
-  teamInfo.style.display = 'block';
-});
-
-teamElement.addEventListener('mouseleave', () => {
-  const teamInfo = teamElement.querySelector('.team-info');
-  teamInfo.style.display = 'none';
-});
-}  
